7e518681d898005afb547069e084603b51987d14
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-09-11 13:49:04 -0700

    cors implement

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const port = 8000;
 
 //Midleware for parsing JSON
-app.use(cors());
+app.use(cors({
+    origin:["https://i-note-book-chi.vercel.app"],
+    method:["POST","PUT","GET","DELETE"],
+    credentials:true
+}));
 app.use(express.json());
 
 // Routes 
